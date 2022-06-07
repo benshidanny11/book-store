157c1bcc81a3fa6c9f6afbe5e5c532ccc65c5adb
benshidanny11 <benshidanny11@gmail.com>
2022-06-07 17:41:47 +0200

    Resolve linter error

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/main.css';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import store from './store/configureStore';
-import {Provider} from 'react-redux'
-
 
 ReactDOM.createRoot(
   document.getElementById('root'),
 )
   .render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>,
   );
 reportWebVitals();
